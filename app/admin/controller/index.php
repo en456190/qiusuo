54f9f0b22009df62221154059f07412745ed4b42
en456190 <en_456190@sina.com>
2020-04-09 11:25:32 +0800

    creat project qiusuo

---
+++ app/admin/controller/index.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace app\admin\controller;
+
+
+use app\BaseController;
+use think\facade\View;
+
+
+class index extends BaseController
+{
+    public function index()
+    {
+
+        //return 111;
+        return View::fetch('index');
+    }
+
+
+
+}
